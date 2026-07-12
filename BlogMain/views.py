43312986b261ad91e203b091dab6962732f63b36
Mayank <91525599+mayankjaiswall@users.noreply.github.com>
2026-07-12 17:54:29 +0530

    Featured-posts-display and models-modification

--- BlogMain/views.py
+++ BlogMain/views.py
@@ -1,6 +1,14 @@
-from django.http import  HttpResponse 
-
 from django.shortcuts import render   
 
+from blogs.models import Blog, Category
+
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    featured_posts = Blog.objects.filter(is_featured=True).order_by('-created_at')
+    
+    context = {
+        'categories': categories,
+        'featured_posts': featured_posts,
+    }
+
+    return render(request, 'home.html', context)
