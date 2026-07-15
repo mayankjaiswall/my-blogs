1ca9422ac772f2bd09ecb2c093d325a8edb0037f
Mayank <91525599+mayankjaiswall@users.noreply.github.com>
2026-07-15 07:03:24 +0530

    Add posts_by_category view
    Introduce posts_by_category view in blogs.views to fetch 'Published' Blog entries for a given category_id, order them by newest, and render posts_by_category.html. Adds Category lookup and returns a 404 page if the category does not exist. Also adds required imports (redirect, HttpResponse, Blog, Category).

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
 
+from .models import Blog, Category
 # Create your views here.
+
+
+def posts_by_category(request, category_id):
+    #Fetch the blogs for the given category_id from the database
+    blogs = Blog.objects.filter(category_id=category_id, status='Published').order_by('-created_at')
+    
+    try:
+        category = Category.objects.get(id=category_id)
+    except Category.DoesNotExist:
+        return render(request, '404.html', status=404)  # Return 404 page if category does not exist
+    
+    context = {
+        'blogs': blogs,
+        'category': category
+    }
+    
+    return render(request, 'posts_by_category.html',context)
