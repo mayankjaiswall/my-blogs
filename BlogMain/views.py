e62790ffe40c7f4907aae7b8f697b38a4c887972
Mayank <91525599+mayankjaiswall@users.noreply.github.com>
2026-07-21 08:21:10 +0530

    Update views.py

--- BlogMain/views.py
+++ BlogMain/views.py
@@ -3,14 +3,12 @@ from django.shortcuts import render
 from blogs.models import Blog, Category
 
 def home(request):
-    categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True, status='Published').order_by('-created_at')
     posts = Blog.objects.filter(status='Published').order_by('-created_at')[:2]  # Limit to the latest 2 posts
 
 
     context = {
-        'categories': categories,
-        'featured_posts': featured_posts,
+        'featured_posts': featured_posts,   
         'posts': posts,
     }
 
