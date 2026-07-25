94e0052e73832cca5854d860a6d82ddb42581151
Mayank <91525599+mayankjaiswall@users.noreply.github.com>
2026-07-25 09:52:07 +0530

    Add socialPlatform app and About context to home
    Register the socialPlatform app in INSTALLED_APPS and integrate About model data into the home view. The home view now fetches the About instance and passes it to the template context for rendering.

--- BlogMain/views.py
+++ BlogMain/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render   
 
+
+from abouts.models import About
 from blogs.models import Blog, Category
 
 def home(request):
@@ -7,9 +9,14 @@ def home(request):
     posts = Blog.objects.filter(status='Published').order_by('-created_at')[:2]  # Limit to the latest 2 posts
 
 
+    try:
+        about = About.objects.get()  # Assuming there's only one About instance
+    except:
+        about = None  # Handle the case where no About instance exists
     context = {
         'featured_posts': featured_posts,   
         'posts': posts,
+        'about': about
     }
 
     return render(request, 'home.html', context)
