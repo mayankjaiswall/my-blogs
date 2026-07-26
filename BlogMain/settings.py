ff2d14aea90ef973ecc0867886f81a1ac3cb7747
Mayank <91525599+mayankjaiswall@users.noreply.github.com>
2026-07-26 16:57:39 +0530

    Add social context processor and blog search URL
    Register blogs.context_processor.get_social_platforms in TEMPLATES context processors and add a blog search route (blogs/search/) to BlogMain.urls. Minor CSS tweak: commented out padding on .sidebar-search-form .form-control. Add abouts migration to set About model verbose_name_plural. These changes expose social platforms to templates, enable blog searching, adjust search form styling, and update About admin display.

--- BlogMain/settings.py
+++ BlogMain/settings.py
@@ -65,7 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'blogs.context_processor.get_categories'
+                'blogs.context_processor.get_categories',
+                'blogs.context_processor.get_social_platforms'
             ],
         },
     },
--- BlogMain/urls.py
+++ BlogMain/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('category/', include('blogs.urls')),
     path('<slug:slug>/',Blogviews.blog_detail, name='blog_detail'),
+    path('blogs/search/', Blogviews.search, name='search'),   
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
