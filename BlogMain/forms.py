2cb603fcae7515414b1ba779f5db364d13273090
Mayank <91525599+mayankjaiswall@users.noreply.github.com>
2026-07-30 08:26:28 +0530

    forms-blog-logindone

--- BlogMain/forms.py
+++ BlogMain/forms.py
@@ -1,8 +1,32 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django import forms
 
 
 class RegistrationForm(UserCreationForm):
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email__iexact=email).exists():
+            raise forms.ValidationError('A user with this email already exists.')
+        return email
+
     class Meta:
         model = User
         fields = ('email', 'username', 'password1', 'password2')
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(
+        label='Username or email',
+        widget=forms.TextInput(attrs={'autofocus': True})
+    )
+
+    def clean(self):
+        username_or_email = self.cleaned_data.get('username')
+
+        if username_or_email and '@' in username_or_email:
+            user = User.objects.filter(email__iexact=username_or_email).first()
+            if user:
+                self.cleaned_data['username'] = user.get_username()
+
+        return super().clean()
